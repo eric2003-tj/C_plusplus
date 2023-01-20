688ddf82becd6a87def192b2a894f59737565299
Eric <eric.tj1026@gmail.com>
2023-01-20 13:57:56 +0800

    more auto

--- content.cpp
+++ content.cpp
@@ -5,18 +5,48 @@
 using namespace std;
 using namespace std::literals;
 int main(){
-   auto str{"string"s};
-   auto num{42};
-   cout << str + "sss"s << endl;
-   cout << num+5 << endl;
-   vector<int> vec = {1,2,6,8,9,2,5};
-   for(auto it = vec.begin(); it!= vec.end();it++){
+   // New forms of begin() and end()
+   vector<int> vec = {1,2,3,4,5};
+   for(auto it = vec.begin();it != vec.end();it++){
+      cout << *it << ' ';
+   }
+   cout << endl;
+   for(auto it = vec.rbegin();it != vec.rend();it++){
+      cout << *it << ' ';
+   }
+   cout << endl;
+   for(auto it = vec.cbegin();it != vec.cend();it++){
+      cout << *it << ' ';
+   }
+   cout << endl;
+   for(auto it = vec.crbegin();it != vec.crend();it++){
+      cout << *it << ' ';
+   }
+   cout << endl;
+   // Global begin() and end()
+   int arr[5] = {1,2,3,4,5};
+   for(auto it = begin(arr);it!=end(arr);it++){
      cout << *it << ' ';
    }
    cout << endl;
-   const int& pi = 5;
-   auto y = pi;
-   cout << y+1 << endl;
-   const auto& j = pi;
-   j++; // appearing error
+   // Range for loops
+   for(auto it:vec){
+     cout << it << ' ';
+   }
+   cout << endl;
+   for(auto &it:vec){
+     it += 2;
+     cout << it << ' ';
+   }
+   cout << endl;
+   // When to use range for loops
+   for(auto it = vec.begin();it != vec.end();it++){
+      if(*it % 2 == 1){
+         vec.erase(it);
+         it--;
+      }
+   }
+   for(auto it = vec.begin();it != vec.end();it++){
+      cout << *it << ' ';
+   }
 }
