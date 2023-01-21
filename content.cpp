778742bf95ea4adb505d0b02f1e3dc6a70f39a63
Eric <eric.tj1026@gmail.com>
2023-01-21 20:20:29 +0800

    string

--- content.cpp
+++ content.cpp
@@ -3,19 +3,23 @@
 #include <string>
 #include <cstdint>
 using namespace std;
-void func(int a,int b){
-  cout << a << " + " << b << " = " << a+b;
-  return ;
-}
-using pfunc = void(*)(int,int);
-pfunc other_func(){
-   return &func;
-}
 int main(){
-   int a{5};
-   int b{3};
-   auto fun_ptr = &func;
-   (*fun_ptr)(a,b);
-   cout << endl;
-   (*other_func())(a,b);
+   string s1{"hello"};
+   string s2{" world"};
+   s1 += s2;
+   cout << s1 << endl;
+   cout << (s1 > s2) << endl;
+   const char* cstr = s1.c_str();
+   cout << cstr << endl;
+   string str {"Hello world"};
+   str[1] = 'a';
+   string st1 = str.substr(6);
+   string st2 = str.substr(6,2);
+   cout << "st1 is :" << st1 << endl;
+   cout << "st2 is :" << st2 << endl;
+   string triplex(3, 'x');
+   cout << triplex << endl;
+   string temp(str,1,5);
+   cout << temp << endl;
+   return 0;
 }
