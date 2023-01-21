bc8a2a2d50f494b05c9df36f582b078c487a4ce7
Eric <eric.tj1026@gmail.com>
2023-01-21 20:35:33 +0800

    find()

--- content.cpp
+++ content.cpp
@@ -4,22 +4,28 @@
 #include <cstdint>
 using namespace std;
 int main(){
-   string s1{"hello"};
-   string s2{" world"};
-   s1 += s2;
-   cout << s1 << endl;
-   cout << (s1 > s2) << endl;
-   const char* cstr = s1.c_str();
-   cout << cstr << endl;
-   string str {"Hello world"};
-   str[1] = 'a';
-   string st1 = str.substr(6);
-   string st2 = str.substr(6,2);
-   cout << "st1 is :" << st1 << endl;
-   cout << "st2 is :" << st2 << endl;
-   string triplex(3, 'x');
-   cout << triplex << endl;
-   string temp(str,1,5);
-   cout << temp << endl;
-   return 0;
+   string str{"Hello world"};
+   if(str.find('o') != string::npos){
+      cout << str.find('o') << endl;
+   }
+   if(str.find("or") != string::npos){
+      cout << str.find("or") << endl;
+   }
+   if(str.find('O') != string::npos){
+      cout << str.find('O') << endl;
+   }
+   if(str.rfind('o') != string::npos){
+      cout << str.rfind('o') << endl;
+   }
+   if(str.rfind("or") != string::npos){
+      cout << str.rfind("or") << endl;
+   }
+   if(str.rfind('O') != string::npos){
+      cout << str.rfind('O') << endl;
+   }
+   string vowels {"aeiou"};
+   cout << str.find_first_of(vowels) << endl;
+   cout << str.find_last_of(vowels) << endl;
+   cout << str.find_first_not_of(vowels) << endl;
+   cout << str.find_last_not_of(vowels) << endl;
 }
