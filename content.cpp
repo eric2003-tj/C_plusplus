5614aab8116d1b9bb2256a05281f2fdb9d024760
Eric <eric.tj1026@gmail.com>
2023-01-24 11:23:57 +0800

    swap data

--- content.cpp
+++ content.cpp
@@ -4,27 +4,16 @@
 #include <cstdint>
 using namespace std;
 int main(){
-   #ifdef to_string()
-     string pi{to_string(3.1415926)};
-     cout << pi << endl;
-   #endif // to_string
-  #ifdef stoi()
-     size_t n; // 定義物件的大小
-     auto i = stoi("12345",&n);
-     cout << n << endl;
-     string str{"42.56"};
-     cout << stoi(str) << endl;
-   #endif // stoi
-   #ifdef error_handling
-     size_t n_proceed;
-     string str{"1235  666666"};
-     auto i = stoi(str,&n_proceed);
-     if(n_proceed < str.size()){
-        cout << "The first non-numeric " << n_proceed << endl;
-     }
-   #endif // error_handling
-   #ifdef Base Conversion
-     int a = stoi("2a",nullptr,16);
-     cout << a << endl;
-   #endif // Base
+   #ifdef data()
+      string str{"20202"};
+      cout << str.data() << endl;
+   #endif // data
+   #ifdef swap
+      string str1{"Hello"};
+      string str2{"Goodbye"};
+      str1.swap(str2);
+      cout << str1 << endl;
+      swap(str1,str2);
+      cout << str1 << endl;
+   #endif // swap
 }
