9cc2b2eba805bacf626e20e52c852ed3d08d1884
Eric <eric.tj1026@gmail.com>
2023-01-24 14:37:27 +0800

    ch func

--- content.cpp
+++ content.cpp
@@ -4,16 +4,32 @@
 #include <cstdint>
 #include <cctype>
 using namespace std;
-void convert(string& str){
-    for(auto it=str.begin();it!=str.end();it++){
-        if(ispunct(*it)){
-            *it = '!';
-        }
+bool equal_strings(const string& lhs, const string& rhs) {
+    if (lhs.size() != rhs.size())
+      return false;
+    auto lit=lhs.cbegin();
+    auto rit = rhs.cbegin();
+    while (lit != lhs.cend() && rit != rhs.cend()) {
+        if (toupper(*lit) != toupper(*rit))
+        return false;
+        ++lit;
+        ++rit;
     }
+    return true;
 }
 int main(){
-   //assignment 4
    string str{"To be, or not to be, that is the question:"};
-   convert(str);
-   cout << str;
+   for(auto &it:str){
+     if(isupper(it)){
+        cout << it << " is an uppercase ch\n";
+     }else if(islower(it)){
+        cout << it << " is a lowercase ch\n";
+     }else if(isdigit(it)){
+        cout << it << " is a digit\n";
+     }else if(isspace(it)){
+        cout << R"(' ')" << " is a digit\n";
+     }else if(ispunct(it)){
+        cout << it << " is a punctuation\n";
+     }
+   }
 }
