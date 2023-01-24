225b821ad21d10e373008b1021c4c77c0fc25181
Eric <eric.tj1026@gmail.com>
2023-01-24 10:54:13 +0800

    str to int

--- content.cpp
+++ content.cpp
@@ -4,21 +4,27 @@
 #include <cstdint>
 using namespace std;
 int main(){
-   #ifdef erase()
-      string str{"hello"};
-      str.erase(1,2); // indexes
-      cout << str << endl;
-      str.assign("hello");
-      str.erase(str.begin(),str.begin()+2);
-      cout << str << endl;
-   #endif // erase
-   #ifdef replace()
-      string str{"hello"};
-      str.replace(1,3,"lllllll"); // replace three elements
-      cout << str << endl;
-      str.assign("hello");
-      str.replace(str.begin()+1,str.end()-2,"llllllll");
-      cout << str << endl;
-   #endif // replace
-
+   #ifdef to_string()
+     string pi{to_string(3.1415926)};
+     cout << pi << endl;
+   #endif // to_string
+  #ifdef stoi()
+     size_t n; // 定義物件的大小
+     auto i = stoi("12345",&n);
+     cout << n << endl;
+     string str{"42.56"};
+     cout << stoi(str) << endl;
+   #endif // stoi
+   #ifdef error_handling
+     size_t n_proceed;
+     string str{"1235  666666"};
+     auto i = stoi(str,&n_proceed);
+     if(n_proceed < str.size()){
+        cout << "The first non-numeric " << n_proceed << endl;
+     }
+   #endif // error_handling
+   #ifdef Base Conversion
+     int a = stoi("2a",nullptr,16);
+     cout << a << endl;
+   #endif // Base
 }
