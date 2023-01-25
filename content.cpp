33c41f260ea6696ac6ab8a26fbd00a4aec9e1ceb
Eric <eric.tj1026@gmail.com>
2023-01-25 17:11:04 +0800

    iomanip

--- content.cpp
+++ content.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
 #include <limits>
-
+#include <iomanip>
 using namespace std;
 
 int main() {
-	int x{0};
-
-	cout << "Please enter a number: ";
-	cin >> x;
-
-	bool success{false};
-	while (!success) {
-		if (cin.good()) {
-			cout << "You entered the number " << x << "\n";
-			success = true;
-		}
-		else if (cin.fail()) {
-			cout << "Please try again and enter a number\n";
-			cin.clear();
-			//cin.ignore(20, '\n');                // Remove next 20 characters or everything up to next newline
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cin >> x;
-		}
-	}
+	cout << boolalpha << (2==3) << endl;
+	cout <<left<< setw(15) << "My dog:" << "Brenden";
+	cout << endl;
+	cout << right;
+	cout << setfill('#');
+	cout << setw(15) << "My dad" << "Yellow";
 }
