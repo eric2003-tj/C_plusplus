8f67bde0c9a3111e2d98b6bc0014d7a25c9e6881
Eric <eric.tj1026@gmail.com>
2023-01-25 11:27:47 +0800

    modes

--- content.cpp
+++ content.cpp
@@ -6,12 +6,9 @@
 #include <fstream>
 using namespace std;
 int main(){
-   const int file_size{10};
-   char buffer[file_size];
-   ifstream ifile{"input.txt"};
-   ifile.read(buffer,file_size);
-   auto nread = ifile.gcount();
-   ifile.close();
-   cout << nread << endl;
-   cout.write(buffer,file_size);
+   ofstream ofile;
+   ofile.open("input.txt",fstream::app);
+   ofile << "\n123456789";
+   ofile.close();
+
 }
