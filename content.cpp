345e1cc57f5567e8c22d250aaee940b56ce3c3d1
Eric <eric.tj1026@gmail.com>
2023-01-25 10:13:44 +0800

    without flush

--- content.cpp
+++ content.cpp
@@ -8,9 +8,12 @@ using namespace std;
 int main(){
    ofstream ofile{"text_out.txt"};
    if(ofile){
-     vector<string> vec = {"dddd","xxxxx","apple"};
-     for(auto v:vec){
-        ofile << v << ",";
+     for(int i=0;i<666666;i++){
+        cout << i << '\n';
+        ofile << i << '\n';
+        if(i==10000){
+            terminate();
+        }
      }
      ofile.close();
    }
