a7105321d6db23f98e18d14517b77d7296954555
Eric <eric.tj1026@gmail.com>
2023-01-25 19:38:03 +0800

    float precision

--- content.cpp
+++ content.cpp
@@ -4,10 +4,25 @@
 using namespace std;
 
 int main() {
-	cout << boolalpha << (2==3) << endl;
-	cout <<left<< setw(15) << "My dog:" << "Brenden";
-	cout << endl;
-	cout << right;
-	cout << setfill('#');
-	cout << setw(15) << "My dad" << "Yellow";
+    #ifdef float
+      double pi {3.1415926535};
+      cout << pi << endl;
+      double c {299'792'458};
+      cout << c << endl;
+    #endif // float
+    #ifdef scientific notation
+      double pi {3.1415926535};
+      cout << scientific << pi << endl;
+      cout << scientific << uppercase << pi << endl;
+    #endif // scientific
+    #ifdef fixed
+      double c {299'792'458};
+      cout << fixed << c << endl;
+      double e {1.602e-19};
+      cout << fixed << e << endl;
+    #endif // fixed
+    double pi {3.1415926535};
+    cout << pi << endl;
+    cout << setprecision(3) << pi << endl;
+
 }
