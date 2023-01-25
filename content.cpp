61cffa6b6091ffe5fdd0f8a176a9671d39e40dd1
Eric <eric.tj1026@gmail.com>
2023-01-25 11:05:03 +0800

    get and put

--- content.cpp
+++ content.cpp
@@ -6,15 +6,8 @@
 #include <fstream>
 using namespace std;
 int main(){
-   ofstream ofile{"text_out.txt"};
-   if(ofile){
-     for(int i=0;i<666666;i++){
-        cout << i << '\n' << flush;
-        ofile << i << '\n' << flush; // equivalent to endl func
-        if(i==10000){
-            terminate();
-        }
-     }
-     ofile.close();
+   char c;
+   while(cin.get(c)){
+      cout.put(c);
    }
 }
