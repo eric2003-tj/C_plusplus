bf259ac4a41d365372ec2315f623152d9ee164b7
Eric <eric.tj1026@gmail.com>
2023-01-25 10:16:10 +0800

    with flush

--- content.cpp
+++ content.cpp
@@ -9,8 +9,8 @@ int main(){
    ofstream ofile{"text_out.txt"};
    if(ofile){
      for(int i=0;i<666666;i++){
-        cout << i << '\n';
-        ofile << i << '\n';
+        cout << i << '\n' << flush;
+        ofile << i << '\n' << flush; // equivalent to endl func
         if(i==10000){
             terminate();
         }
