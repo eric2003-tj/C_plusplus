78aa7d6b72b81cfe176a11cd60e371145e8287b1
Eric <eric.tj1026@gmail.com>
2023-01-26 10:48:58 +0800

    ostream_iterator()

--- content.cpp
+++ content.cpp
@@ -4,27 +4,13 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <iterator>
 using namespace std;
 
 int main() {
-    ostringstream output;                                   // Open an output stringstream
-
-	string data{"It is time to say "};
-    output << data;                                         // Write some data to the stream
-
-    auto marker = output.tellp();                           // Save current file position marker
-	cout << data.size() << " characters written to stream\n";
-	cout << "Stream marker is " << marker << " bytes into the stream\n";
-
-	output << "hello";                                      // Write some more data to the stream
-	cout << "Stream marker is now " << output.tellp() << " bytes into the stream\n";
-
-	cout << output.str() << endl;
-
-    if (marker != -1)                                       // We have changed our mind!
-        output.seekp(marker);                               // Move marker back to original position
-
-	output << "goodnight";                                  // Write some data to stream
-
-	cout << output.str() << endl;
+    ostream_iterator<int> oi(cout,"\n");
+    for(int i=0;i<10;i++){
+        *oi = i;
+        oi++;
+    }
 }
