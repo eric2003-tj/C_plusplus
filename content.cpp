bb155801b5a4771c58e57d41b6d5dfbc4322337a
Eric <eric.tj1026@gmail.com>
2023-01-26 10:35:45 +0800

    random access stream

--- content.cpp
+++ content.cpp
@@ -7,17 +7,24 @@
 using namespace std;
 
 int main() {
-    ifstream ifile("input.txt");
-    string text;
-    vector<int> vec;
-    while(getline(ifile,text)){
-        istringstream in(text);
-        int num;
-        while(in >> num){
-            vec.push_back(num);
-        }
-    }
-    for(auto n:vec){
-        cout << n << endl;
-    }
+    ostringstream output;                                   // Open an output stringstream
+
+	string data{"It is time to say "};
+    output << data;                                         // Write some data to the stream
+
+    auto marker = output.tellp();                           // Save current file position marker
+	cout << data.size() << " characters written to stream\n";
+	cout << "Stream marker is " << marker << " bytes into the stream\n";
+
+	output << "hello";                                      // Write some more data to the stream
+	cout << "Stream marker is now " << output.tellp() << " bytes into the stream\n";
+
+	cout << output.str() << endl;
+
+    if (marker != -1)                                       // We have changed our mind!
+        output.seekp(marker);                               // Move marker back to original position
+
+	output << "goodnight";                                  // Write some data to stream
+
+	cout << output.str() << endl;
 }
