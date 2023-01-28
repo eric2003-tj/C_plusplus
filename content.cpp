abc6b3ce05951350cf4c810c8e66f638f4224d3d
Eric <eric.tj1026@gmail.com>
2023-01-28 10:08:17 +0800

    assignment operator

--- content.cpp
+++ content.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
-#include <limits>
-#include <iomanip>
-#include <sstream>
-#include <fstream>
-#include <vector>
-#include <iterator>
-#include <cstdint>
+#include <string>
+
 using namespace std;
-class Test {
-    public:
-        int i;
-        string str;
-    // Class interface function declarations
-    // Compiler-generated copy constructor
-    // Initializes "this" by copying the argument's i member
-    // and calling std::string's copy constructor for y
-    // Test(const Test& other) : i(other.i), y(other.y) {}
-    Test(int x,string new_str){
-       i = x;
-       str = new_str;
-    }
-    Test(const Test& other){
-       i = other.i;
-       str = other.str;
-    }
+
+class String {
+	string s;
+public:
+	// Constructors
+	String(): s("") {}
+	String(const string& s): s(s) {}
+
+	// Copy constructor
+	String(const String& arg): s(arg.s) {}
+
+	// Assignment Operator
+	String& operator =(const String& arg) {
+		s = arg.s;
+		return *this;
+	}
+
+	void print() { cout << s << endl; }
 };
 
 int main() {
-    Test nee{5,"hello"};
-    Test trial{nee};
-    cout << trial.i << endl;
-    cout << trial.str << endl;
+	String w{"world"};
+	String bang{"bang"};
+	bang = w;
+
+	cout << "w = "; w.print();
+	cout << "bang = "; bang.print();
 }
