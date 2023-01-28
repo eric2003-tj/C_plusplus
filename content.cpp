cc5f1aec35bf28b5e6c016000bf06942ebf1718f
Eric <eric.tj1026@gmail.com>
2023-01-28 10:47:26 +0800

    shallow and deep copy

--- content.cpp
+++ content.cpp
@@ -3,31 +3,67 @@
 
 using namespace std;
 
-class Test {
-	int i;
-	string str;
+class String {
+	char *data;          // Array member to store the characters
+	int size;            // The number of elements in the array
 public:
-	// Test(int i, const string& s) : i(i), str(s) {}
-	// Test() {}
-	// Test(const Test& other) : i(other.i), str(other.str) {}
-	// Test& operator =(const Test& other) { i = other.i; str = other.str; }
-	// ~Test() {}
-	void print() { cout << "i = " << i << ", str = " << str << endl; }
+	// Constructor
+	//shallow copy
+	String(const std::string& s) : size(s.size()) {
+		data = new char[size];              // Allocate the heap memory for the data
+
+
+		for (int i = 0; i < size; ++i)      // Populate the data
+			data[i] = s[i];
+	}
+    //deep copy
+	String(const String& arg) : size(arg.size) {
+		cout << "Calling copy constructor\n";
+		data = new char[size];              // Allocate the heap memory for arg's data
+
+		for (int i = 0; i < size; ++i)      // Populate the memory with arg's data
+			data[i] = arg.data[i];
+	}
+
+	// Assignment Operator
+	String& operator =(const String& arg) {
+		cout << "Calling assignment operator\n";
+
+		if (&arg != this) {                    // Check for self-assignment
+			cout << "Reallocating memory\n";
+			delete [] data;                    // Release the original memory
+			data = new char[arg.size];         // Allocate the data member
+
+			size = arg.size;                   // Assign to the size member
+
+			for (int i = 0; i < size; ++i)     // Populate the data
+				data[i] = arg.data[i];
+		}
+
+		return *this;                                  // Return the assigned-to object
+	}
+
+	// Destructor
+	~String() {
+		cout << "Calling destructor" << endl;
+
+		delete [] data;                     // Release the heap memory for the data
+	}
+
+	int length() {
+		return size;
+	}
 };
 
 int main() {
-	Test test;
-	//Test test(5, "Hello"s);
-	cout << "test after default constructor: ";
-	test.print();
-	//Test test2(7, "Goodbye"s);
-	Test test2;
-	cout << "test2 after default constructor: ";
-	test2.print();
-	Test test3 = test;
-	cout << "test3 after copy constructor: ";
-	test3.print();
-	test = test2;
-	cout << "test after assignment operator: ";
-	test.print();
+	String str("1"s);
+
+		String str2("Two"s);
+		String str3(str2);
+		cout << "Str2 size: " << str2.length() << endl;
+		cout << "Str3 size: " << str3.length() << endl;
+		str2 = str3;
+		cout << "Str2 size: " << str2.length() << endl;
+
+	cout << "Str size: " << str.length() << endl;
 }
