0dca233064e328107b4333ef031309555bfdbd2b
Eric <eric.tj1026@gmail.com>
2023-01-31 09:59:30 +0800

    implicit cast

--- content.cpp
+++ content.cpp
@@ -3,22 +3,15 @@
 #include <string>
 #include <cstdint>
 using namespace std;
+class Test{
+   int i;
+   public:
+       Test(int d){
+           i = d;
+       }
+       operator int() const{return i;}
+};
 int main(){
-   #ifdef erase()
-      string str{"hello"};
-      str.erase(1,2); // indexes
-      cout << str << endl;
-      str.assign("hello");
-      str.erase(str.begin(),str.begin()+2);
-      cout << str << endl;
-   #endif // erase
-   //#ifdef replace()
-      string str{"hello"};
-      str.replace(1,3,"xxxxxxxx"); // replace three elements
-      cout << str << endl;
-      str.assign("hello");
-      str.replace(str.begin()+1,str.end()-2,"llllllll");
-      cout << str << endl;
-   //#endif // replace
-
+   Test ob{7};
+   cout << ob << endl;
 }
