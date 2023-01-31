2e030aafe85792e3bc215ed236523edca1a345b5
Eric <eric.tj1026@gmail.com>
2023-01-31 19:55:07 +0800

    functor

--- content.cpp
+++ content.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 class Test{
    public:
-       Test() = default;
-       Test(const Test&) = delete;                    // Deleted copy constructor
-       Test& operator =(const Test&) = delete;        // Deleted assignment operator
+       bool operator()(int n){
+          return (n%3==0);
+       }
 };
 int main(){
-    Test t1, t2;                                   // Use defaulted constructor
-	Test t3(t1);                                   // Error: use of deleted function
-	t2 = t1;
+    Test t;
+    if(t(3)){
+        cout << "ff";
+    }
 }
