df451b2dd87f8263edcb4b821c18efebf61d2167
Eric <eric.tj1026@gmail.com>
2023-02-02 11:42:17 +0800

    add methods

--- content.cpp
+++ content.cpp
@@ -5,7 +5,15 @@
 #include <algorithm>
 using namespace std;
 int main(){
-   pair<string,int> x{"Hello"s,150};
-   cout << x.first << endl;
-   cout << x.second << endl;
+   string str{"Hello world!"};
+   string vowel{"aeiou"};
+   cout << str.find_first_of(vowel) << endl;
+   auto pos = adjacent_find(cbegin(str),cend(str));
+   cout << *pos << endl;
+   vector<int> vec = {1,1,2,2,3,3,3,3};
+   auto pos2 = search_n(cbegin(vec),cend(vec),4,3);
+   cout << *pos2 << endl;
+   string temp{"wor"};
+   auto pos3 = search(cbegin(str),cend(str),cbegin(temp),cend(temp));
+   cout << *pos3 << endl;
 }
