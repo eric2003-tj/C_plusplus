b01b349474981e487ec928814c9322524ffd27d5
Eric <eric.tj1026@gmail.com>
2023-02-03 10:09:37 +0800

    copy_n

--- content.cpp
+++ content.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
    vector<int> vec = {1,2,3,4,5};
    vector<int> vec2(vec.size());
-   copy(cbegin(vec),cend(vec),begin(vec2));
+   copy_n(cbegin(vec),2,begin(vec2));
    for(auto n: vec2){
       cout << n << endl;
    }
